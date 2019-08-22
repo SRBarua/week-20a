50ad90d7e88589a99ffc4a328e3b0bd5d9288aca
SRBarua <baruasudipta@gmail.com>
2019-08-22 16:35:23 -0600

    week20a

---
+++ src/components/GifSearch.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div>
+            <div>
+                <form @submit.prevent="doSearch">
+                    <input v-model="query" type="text" placeholder="GIF Search" />
+                    <button type="submit" class="btn btn-primary">Go</button>
+                </form>
+            </div>
+            <div>
+                <img v-for="result in results" :src="result.images.fixed_height.url" />
+            </div>
+        </div>
+    </div>
+
+</template>
+<script>
+    import axios from 'axios'
+    export default {
+        data() {
+            return {
+                apiKey: 'EXUoxgZA4Ksq3OvB9o75z0mHlrQqSUse',
+                query: '',
+                results: [],
+            }
+        },
+
+        methods: {
+            doSearch() {
+                axios.get( 'https://api.giphy.com/v1/gifs/search?api_key=' + this.apiKey + '&q=' + this.query)
+                    .then ((response) => {
+                        this.results = response.data.data;    
+                    })
+            }
+    }
+}
+</script>
+
+
+<style lang="scss">
+</style>
